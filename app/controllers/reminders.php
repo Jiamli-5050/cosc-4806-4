5b0b92efb23ddaf9fccd59d00a03278c0561404a
Jiamli-5050 <jiamli@algomau.ca>
2025-06-30 01:26:12 +0000

    connecting the reminders with the new db table

--- app/controllers/reminders.php
+++ app/controllers/reminders.php
@@ -3,7 +3,11 @@
 class Reminders extends Controller {
 
    public function index() {
+     echo 1;
+     $reminder = $this->model('Reminder');
+     $list_of_reminders = $reminder->get_all_reminders();
+     print_r ($list_of_reminders);
+     die;
      $this->view('reminders/index');
    }
 }
-?>
