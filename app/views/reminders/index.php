23f42e239841f57fa33fc810346ece5df4ceeedf
Jiamli-5050 <jiamli@algomau.ca>
2025-06-30 02:15:23 +0000

    fix bugs on reminder page

--- app/views/reminders/index.php
+++ app/views/reminders/index.php
@@ -4,13 +4,18 @@
     <div class="row">
       <div class="col-lg-12">
         <h1>Reminders</h1>
-        <p a href="/reminders/create" class="btn btn-primary">Create Reminder</p>
+        <a href="/reminders/create" class="btn btn-primary">Create Reminder</a>
       </div>
     </div>  
   </div>
 
   <?php
-  print_r($data['reminders']);
-    ?>
+  foreach ($data['reminders'] as $reminder) : ?>
+    <p>
+      <?= htmlspecialchars($reminder['subject']) ?>
+      <a href="/reminders/update?id=<?= $reminder['id'] ?>">update</a>
+      <a href="/reminders/delete?id=<?= $reminder['id'] ?>">delete</a>
+    </p>
+   <?php endforeach; ?>
 
-<?php require_once 'app/views/templates/footer.php'?>
+<?php require_once 'app/views/templates/footer.php' ?>
