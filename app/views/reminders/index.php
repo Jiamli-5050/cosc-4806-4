eb48302ffaf5a0d90fb328311800eaca7d5aba16
Jiamli-5050 <jiamli@algomau.ca>
2025-07-01 18:12:15 +0000

    moved the create reminder to the reminders page

--- app/views/reminders/index.php
+++ app/views/reminders/index.php
@@ -22,25 +22,4 @@ require_once 'app/views/templates/header.php' ?>
     </p>
    <?php endforeach; ?>
 
-  <h3> Create a reminder</h3>
-  <form method="post" action="/reminders/store">
-    <table class="table table-bordered mt-3">
-      <thead>
-        <tr>
-          <th> New Reminder</th>
-          <th style ="width: 100px">Action </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>
-            <input type="text" name="subject" class="form-control" placeholder="Enter reminder" required>
-          </td>  
-          <td>
-            <button type="submit" class="btn btn-primary">Create</button>
-       </td>
-    </tr>
-  </tbody>
- </table>
-</form>
 <?php require_once 'app/views/templates/footer.php' ?>
