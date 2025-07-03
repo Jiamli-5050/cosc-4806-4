5abc7dc717e7e7387405f2903c231d5fc4fa951c
Jiamli-5050 <jiamli@algomau.ca>
2025-07-03 01:41:00 +0000

    made the reminders page more user friendly

--- app/views/reminders/index.php
+++ app/views/reminders/index.php
@@ -13,16 +13,28 @@ require_once 'app/views/templates/header.php' ?>
     </div>  
   </div>
 
-  <?php
-  foreach ($data['reminders'] as $reminder) : ?>
-    <p>
-      <?= htmlspecialchars($reminder['subject']) ?>
-      <a href="/reminders/update?id=<?= $reminder['id'] ?>">update</a>
+  <table class="table table-bordered mt-3">
+    <thead>
+      <tr>
+        <th>Reminder</th>
+        <th style="width: 180px;">Action</th>
+      </tr>
+    </thead>
+  <tbody>
+
+  <?php foreach ($data['reminders'] as $reminder) : ?>
+    <tr>
+      <td><?= htmlspecialchars($reminder['subject']) ?></td>
+      <td>
+      <a href="/reminders/update?id=<?= $reminder['id'] ?>" class="btn btn-sm btn-warning">Update</a>
       <form method ="post" action="/reminders/delete" style="display: inline;">
         <input type="hidden" name="id" value="<?= $reminder['id'] ?>">
-        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">delete</button>
+        <button type="submit" class="btn btn-sm btn-danger">Delete</button>
       </form>
-    </p>
+      </td>
+  </tr>
    <?php endforeach; ?>
+  </tbody>  
+  </table>
 
 <?php require_once 'app/views/templates/footer.php' ?>
